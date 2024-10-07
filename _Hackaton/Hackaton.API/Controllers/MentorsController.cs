0dbccd23f632890d7ac533dbd018f1d9b302a202
sofiaAmaya <laura18amaya@gmail.com>
2024-10-06 21:40:06 -0500

    inicializo controller

---
+++ _Hackaton/Hackaton.API/Controllers/MentorsController.cs
@@ -0,0 +1,19 @@
+﻿using Hackaton.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackaton.API.Controllers
+{
+
+    [ApiController]
+    [Route("/api/mentors")]
+    public class MentorsController: ControllerBase
+    {
+        private readonly DataContext _context;
+        public MentorsController(DataContext context)
+        {
+            _context = context;
+
+
+        }
+    }
+}
